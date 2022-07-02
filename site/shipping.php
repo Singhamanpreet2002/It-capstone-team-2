e7b8026cc3abdea41922723bf1d1cf28c9cd9c9b
Amanpreet Singh <51552560+Singhamanpreet2002@users.noreply.github.com>
2022-07-01 21:01:58 -0400

    Shipping and payment page

---
+++ site/shipping.php
@@ -0,0 +1,144 @@
+<?php 
+session_start();
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shipping and payment</title>
+    <link rel='stylesheet' type='text/css' media='screen' href='css/style.css'>
+    <link rel='stylesheet' type='text/css' media='screen' href='css/shipping.css'>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Kdam+Thmor+Pro&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <nav>
+            <div class="logo">
+                <a href="index.php"><img src="images/Logo.svg"></a>
+            </div>
+            <ul class="nav-links">
+                
+                <li><a href="Services.php">Services</a></li>
+                <li><a href="product.php">Products &#9660;</a>
+                    <ul class="sub-nav-links">
+                        <li><a href="./preBuilt.php">Pre-Builds</a></li>
+                        <li><a href="./buildOwn.php">Build you Own</a></li>
+                    </ul>
+                </li>
+                <li><a href="Contact.php">Contact</a></li>
+                <?php 
+   if (isset($_SESSION['username'])) {
+   echo '<li><a href="logout.php">Logout</a></li>';
+   echo "<div style = 'letter-spacing:3px;font-size: 20px;position: absolute;right: 30px; ' > Hello " . $_SESSION['username'] . "</div>" ;
+}
+  else {
+          echo  '<div class="login">';
+          echo  ' <li><a href="login.php"><img src="images/Login.svg" alt="">Login/Register</a></li></div>';
+  }
+  ?>             
+            </ul>
+            <div class="burger">
+                <div class="line1 line"></div>
+                <div class="line2 line"></div>
+                <div class="line3 line"></div>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <Form>
+            <legend>Summary</legend>
+                <div class="container-vert">
+                    <div class="summary">
+                        <label for="prod">Starter PC </label>
+                        <label for="price">$1599</label>
+                    </div>
+                    <div class="summary">
+                        <label for="prod">Starter pro PC </label>
+                        <label for="price">$1799</label>
+                    </div>
+                    <div class="summary">
+                        <label for="Total">Total </label>
+                        <label for="price">$3398</label>
+                    </div>
+                </div>
+        </Form>
+        <Form>
+            <legend>Shipping</legend>
+            <div class="container">
+                <label for="fname">First Name</label>
+                <input type="text" type="fname" id="fnam">
+            </div>
+            <div class="container">
+                <label for="lname">Last Name</label>
+                <input type="text" type="lname" id="lname">
+            </div>
+            <div class="container">
+                <label for="email">Email Address</label>
+                <input type="email" type="email" id="email">
+            </div>
+            <div class="container">
+                <label for="zipcode">zip code</label>
+                <input type="text" type="zipcode" id="zipcode">
+            </div>
+            <div class="container">
+                <label for="address">Address</label>
+                <input type="text" type="address" id="address">
+            </div>
+            
+        </Form>
+        <Form>
+            <legend>Payment Info</legend>
+            <div class="container">
+                <label for="CName">Card Holder Name</label>
+                <input type="text" type="CName" id="CName">
+            </div>
+            <div class="container">
+                <label for="CNum">Credit Card Number</label>
+                <input type="number" type="CNum" id="CNum">
+            </div>
+            <div class="container">
+                <label for="date">exp date</label>
+                <input type="date" type="date" id="date">
+            </div>
+            <div class="container">
+                <label for="cvv">CVV</label>
+                <input type="number" type="cvv" id="cvv">
+            </div>
+            <div class="container">
+                <label for="address">Billing Address</label>
+                <input type="text" type="address" id="address">
+            </div>
+        </Form>
+
+    </main>
+    <footer>
+        <div class="footer">
+            <div class="foot-logo">
+                <img src="images/foot-svg.svg" alt="">
+            </div>
+            <div class="info">
+                <div class="social-media">
+                    <a href="https://www.facebook.com/" target="_blank"><img src="images/facebook.svg"></a>
+                    <a href="https://twitter.com/" target="_blank"><img src="images/twiter.svg"></a>
+                    <a href="https://www.instagram.com/" target="_blank"><img src="images/instagram.svg"></a>
+                    <a href="https://www.linkedin.com/" target="_blank"><img src="images/Linked_in.svg"></a>
+                </div>
+                <p>2273 Marcus Street, Huntsville. AL</p>
+                <p>256-606-2447</p>
+        </div>
+        <div class="lic">©Binary Bros Inc. 2022 All Rights Reserved</div>
+          </div>
+    </footer>
+    
+</body>
+<script src='Js/main.js'></script>
+</html>
